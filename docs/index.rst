c042e154c661166e6d01ad67857049c94d2c4968
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-08-28 13:52:06 -0400

    Initial Commit

---
+++ docs/index.rst
@@ -0,0 +1,14 @@
+oastools documentation
+======================
+
+Contents
+--------
+
+.. toctree::
+   :maxdepth: 3
+   :numbered:
+
+   installation.rst
+   overview.rst
+   API documentation <source/modules.rst>
+   about.rst
---
+++ tests/test_core.py
@@ -0,0 +1,17 @@
+""" Test of oastools.core
+
+:Author: Name <email>
+:Date: 2019-8-28
+:Copyright: 2019, Bilal Shaikh
+:License: MIT
+"""
+
+from oastools import core
+import unittest
+
+
+class CoreTestCase(unittest.TestCase):
+
+    def test_1(self):
+        # Example test
+        self.assertTrue(True)  # example assertion
