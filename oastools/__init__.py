ba8eb31d9885c96e3d0773175aaf6d6c54b2dc28
Bilal Shaikh <bilalshaikh42@gmail.com>
2019-08-29 18:49:14 -0400

    added package strucuring

--- oastools/__init__.py
+++ oastools/__init__.py
@@ -1,5 +1,10 @@
+from .validate import *
+from .parse import *
+from .utils import *
 import pkg_resources
 
 # read version
 with open(pkg_resources.resource_filename('oastools', 'VERSION'), 'r') as file:
     __version__ = file.read().strip()
+
+#import subpackages
---
+++ oastools/parse/parser.py
@@ -0,0 +1,12 @@
+""" parser
+
+:Author: Bilal Shaikh < bilalshaikh42@gmail.com >
+:Date: 2019-08-29
+:Copyright: 2019, Bilal Shaikh
+:License: MIT
+"""
+
+
+class BaseParser(object):
+    def __init__(self, url=None, spec=None):
+        pass
